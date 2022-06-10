4fafad0217e6453df9e06ffcd2ac591bedaf50db
Aleksei <alex312s12@gmail.com>
2022-06-10 09:39:02 +0600

    Shifted dice images on 1 step

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -25,12 +25,12 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val randomInt = Random().nextInt(6)+1
         val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+            1 -> R.drawable.dice_2
+            2 -> R.drawable.dice_3
+            3 -> R.drawable.dice_4
+            4 -> R.drawable.dice_5
+            5 -> R.drawable.dice_6
+            else -> R.drawable.dice_1
         }
         diceImage.setImageResource(drawableResource)
     }
